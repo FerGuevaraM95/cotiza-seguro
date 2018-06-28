c012ccf6bb22c1bd0f316cb1424d1921b055095c
Fernando Guevara <luisfernandoguevara95@gmail.com>
2018-06-28 17:52:45 -0500

    Add insurance quote metod

--- js/app.js
+++ js/app.js
@@ -1,14 +1,54 @@
 
 // Constructor para seguro
-function insurance(region, year, type) {
+function Insurance(region, year, type) {
     this.region = region;
     this.year = year;
     this.type = type;
 }
 
+Insurance.prototype.insuranceQuote = function() {
+    console.log(this.region);
+    console.log(this.year);
+    console.log(this.type);
 
-// Toda la interfaz
-function UI() {}
+    /*
+        1 = Americano 1.15
+        2 = Asiatico 1.05
+        3 = Europeo 1.35
+    */
+
+    let quantity;
+    const base = 2000;
+
+    switch (this.region) {
+        case "1":
+            quantity = base * 1.15;
+            break;
+        case "2":
+            quantity = base * 1.05;
+            break;
+        case "3":
+            quantity = base * 1.35;
+            break;
+    }
+
+    const difference = new Date().getFullYear() - this.year;
+    // Cada año de diferencia hay que reducir 3% el valor del seguro
+    quantity -= ((difference * 3) * quantity) / 100;
+    /*
+        si el seguro es basico se multiplica por 30%
+    */
+   if(this.type === 'basic') {
+       quantity *= 1.30;
+   } else {
+       quantity *= 1.50;
+   }
+
+    return quantity;
+}
+
+  // Toda la interfaz
+  function UI() {};
 
 UI.prototype.showMessage = function(message, messageType) {
     const div = document.createElement('div');
@@ -54,7 +94,9 @@ form.addEventListener('submit', function(e) {
             interfaz.showMessage('Faltan datos, revisa el formulario y prueba de nuevo', 'error');
     } else {
         // Instancia seguro y mostrar interfaz
-        console.log('Todo correcto');
+        const insurance = new Insurance(selectedRegion, selectedYear, type);
+        // cotizar el seguro
+        const quantity = insurance.insuranceQuote();
     }
 });
 
